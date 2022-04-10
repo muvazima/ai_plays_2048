65eb90112a7f2151cdea6923c2a71acd429b31c5
Sherin Cherian <smcck222@gmail.com>
2022-04-10 01:33:57 +0100

    set epsilon = 0, added args

--- rl_agent/test.py
+++ rl_agent/test.py
@@ -7,13 +7,16 @@ from game_display import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', help='Enter mode(Snake Heuristic/N-Tuples Heuristic')
+parser.add_argument('--trials', help='Number of trials')
+
 args = parser.parse_args()
 
 # Loading the trained agent from best_agent.npy.
-# Execute 100 episodes, replay the best episode.
+# Execute args.trials episodes, replay the best episode.
 
 agent = Q_agent.load_agent("best_agent.npy", args.mode)
 est = agent.evaluate
-results = Game.trial(estimator=est, num=100, mode = args.mode)
+results = Game.trial(estimator=est, num = int(args.trials), mode = args.mode)
 
+# Displays the best game.
 display = Display(matrix = results[0].row)
